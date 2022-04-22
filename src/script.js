3f9c1422f0fad50339d65526f4e297a7d3e22b44
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2022-04-22 16:27:42 -0500

    Transform objects

--- src/script.js
+++ src/script.js
@@ -1,1 +1,72 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
+scene.add(mesh);
+// Normalize fixes the vector position
+// mesh.position.normalize();
+console.log(mesh.position.length());
+
+// mesh.scale.x = 2;
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+mesh.rotation.reorder("YXZ");
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+
+// Group
+const group = new THREE.Group();
+group.position.y = 1;
+scene.add(group);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube2.position.x = -2;
+cube3.position.x = 2;
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+
+// Axes helpers
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+// camera.position.set(1, 1, 3);
+scene.add(camera);
+// camera.lookAt(mesh.position);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("canvas.webgl"),
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
